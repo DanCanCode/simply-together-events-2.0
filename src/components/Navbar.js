e120bff6e9a86678384d6a81cfa65cef29ca1c97
Daniel Espinal <dancancode@protonmail.com>
2022-11-24 15:00:42 -0500

    navbar structure adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const handleResize = () => {
     setWidth(window.innerWidth);
   };
@@ -14,41 +15,165 @@ const Navbar = () => {
   }, []);
 
   const changeDisplay = () => {
-    console.log(width);
     if (width >= 640) {
       return (
         <>
-          <NavLink to="/" className="text-white">
-            Home
+          <NavLink
+            to="/"
+            className="text-white mx-3 xxs:text-sm xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            HOME
           </NavLink>
-          <NavLink to="/about" className="text-white">
-            About
+          <NavLink
+            to="/about"
+            className="text-white mx-3 xxs:text-sm  xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            ABOUT
           </NavLink>
-          <NavLink to="/about" className="text-white">
-            Services
+          <NavLink
+            to="/services"
+            className="text-white mx-3 xxs:text-sm xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            SERVICES
           </NavLink>
-          <NavLink to="/about" className="text-white">
-            Colaboration
+          <NavLink
+            to="/gallery"
+            className="text-white mx-3 xxs:text-sm xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            GALLERY
+          </NavLink>
+          <NavLink
+            to="/collaboration"
+            className="text-white mx-3 xxs:text-sm xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            COLLABORATION
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="text-white mx-3 xxs:text-sm xxs:mx-1.5 xs:text-sm xs:mx-2"
+          >
+            CONTACT
           </NavLink>
         </>
       );
     } else {
-      return <FiMenu className="text-white" />;
+      return (
+        // <button className="flex items-center">
+        //   <FiMenu className="text-white text-3xl text-center" />
+        // </button>
+        <nav>
+          <section className="MOBILE-MENU flex lg:hidden">
+            <div
+              className="HAMBURGER-ICON space-y-2 cursor-pointer"
+              onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+            >
+              <span className="block h-0.5 w-8 bg-white"></span>
+              <span className="block h-0.5 w-8 bg-white"></span>
+              <span className="block h-0.5 w-8 bg-white"></span>
+            </div>
+
+            {isNavOpen && (
+              <div className={`${isNavOpen ? "showMenuNav" : "hideMenuNav"}`}>
+                <div
+                  className="CROSS-ICON absolute top-0 right-0 px-8 py-8 cursor-pointer"
+                  onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+                >
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+
+                <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[150px]">
+                  <li className="">
+                    <NavLink to="/">HOME</NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink to="/about">ABOUT</NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink to="/services">SERVICES</NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink to="/gallery">GALLERY</NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink to="/collaboration">COLLABORATION</NavLink>
+                  </li>
+                  <li className="">
+                    <NavLink to="/contact">CONTACT</NavLink>
+                  </li>
+                </ul>
+              </div>
+            )}
+            {isNavOpen && <div className="navBlur"></div>}
+          </section>
+
+          <style>{`
+            .navBlur {
+                display: block;
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                backdrop-filter: blur(4px);
+                background-color: hsla(0,0%,7%,.36);
+            }
+
+            .hideMenuNav {
+                display: none;
+                transition: .25s;
+            }
+
+            .showMenuNav {
+                display: block;
+                background-color: white;
+                position: absolute;
+                width: 70%;
+                height: 100vh;
+                top: 0;
+                left: 0;
+                z-index: 10;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-evenly;
+                align-items: center;
+            `}</style>
+        </nav>
+      );
     }
   };
 
   return (
     <div className="py-2 bg-black">
-      <div className="flex flex-row-reverse items-center justify-center sm:justify-start sm:flex-row">
-        <div className="justify-self-center  sm:justify-self-start sm:pl-8">
+      <div className="grid grid-cols-3 sm:grid-cols-2 sm:place-items-center">
+        {width < 640 && (
+          <div className="pl-6 self-center">{changeDisplay()}</div>
+        )}
+        <div className="justify-self-center sm:justify-self-start sm:pl-8">
           <Link to="/">
-            <img className="w-20 bg-white" src={logo} alt="site logo" />
+            <img
+              className="w-20 bg-white xxs:w-16"
+              src={logo}
+              alt="site logo"
+            />
           </Link>
         </div>
 
-        <div className="justify-self-start sm:justify-self-end">
-          {changeDisplay()}
-        </div>
+        {width >= 640 && (
+          <div className="sm:justify-self-end sm:pr-8 sm:pl-0">
+            {changeDisplay()}
+          </div>
+        )}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xxs: { min: "640px", max: "720px" },
+      xs: { min: "720px", max: "860px" },
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
