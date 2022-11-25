7daf1af3eb43868b98116dcecd2cc6ac44d34d41
Daniel Espinal <dancancode@protonmail.com>
2022-11-25 18:30:36 -0500

    created photo wall

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,36 +10,51 @@ import photo5 from "../homepagePhotos/photo5.jpg";
 const Homepage = () => {
   return (
     <div>
-      <div className="relative overflow-hidden max-w-full max-h-max mt-12">
-        <div className="static flex items-center overflow-hidden max-w-full">
+      <div className=" overflow-hidden h-[65vh] mt-12">
+        <div className="relative flex items-center max-w-full h-4/6">
           <img
-            className="absolute bottom-auto left-0 w-80 object-cover"
+            className="relative top-10 left-0 w-80 object-cover"
             src={photo0}
             alt="ste1"
           />
           <img
-            className="absolute w-64 bottom-auto left-24 object-cover"
+            className="relative w-60 top-4 right-10 z-20 object-cover"
             src={photo1}
             alt="ste2"
           />
-          <img className="absolute w-64 object-cover" src={photo2} alt="ste3" />
-          <img className="w-96 object-cover" src={photo3} alt="ste4" />
-          <img className="w-64 h-fit object-cover" src={photo4} alt="ste5" />
-          <img className="w-64 h-fit object-cover" src={photo5} alt="ste6" />
+          <img
+            className="relative w-56 top-24 right-16 z-10 object-cover"
+            src={photo2}
+            alt="ste3"
+          />
+          <img
+            className="relative w-96 object-cover top-16 right-32"
+            src={photo3}
+            alt="ste4"
+          />
+          <img
+            className="w-64  object-cover relative z-10 top-20 right-40"
+            src={photo4}
+            alt="ste5"
+          />
+          <img
+            className="w-64 object-cover relative top-4 right-56"
+            src={photo5}
+            alt="ste6"
+          />
         </div>
+      </div>
 
-        <div className="text-center mt-4">
-          <p>
-            simply bringing everyone together the most beautiful way we know
-            how.
-          </p>
-          <Link
-            to="/services"
-            className="mt-4 static p-3 bg-red-800 inline-block text-white rounded-md"
-          >
-            learn more
-          </Link>
-        </div>
+      <div className="text-center mt-4">
+        <p>
+          simply bringing everyone together the most beautiful way we know how.
+        </p>
+        <Link
+          to="/services"
+          className="mt-4 static p-3 bg-red-800 inline-block text-white rounded-md"
+        >
+          learn more
+        </Link>
       </div>
     </div>
   );
