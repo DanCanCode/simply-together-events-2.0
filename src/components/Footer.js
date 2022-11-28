c75aed1efcb7469b0a86fccc48189e98abd746b0
Daniel Espinal <dancancode@protonmail.com>
2022-11-28 18:19:26 -0500

    made homepages mobile responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
     <footer className="bg-zinc-700 mt-6 h-50">
       <section className="grid grid-cols-1 place-items-center">
-        <nav className="flex items-center justify-center gap-4 sm:mt-8 mt-6">
+        <nav className="flex items-center justify-center gap-4 sm:mt-8 xs:mt-6 mt-4">
           <a
             href="https://www.instagram.com/simplytogether.e/"
             target="_blank"
             rel="noopener noreferrer"
-            className="sm:text-lg text-md bg-white/50 p-2 rounded-full flex items-cente text-zinc-700"
+            className="sm:text-lg xs:text-md text-sm bg-white/50 p-2 rounded-full flex items-center text-zinc-700"
           >
             <FaInstagram className="inline-block" />
           </a>
@@ -18,13 +18,13 @@ const Footer = () => {
             href="https://www.pinterest.com/simplytogetherevents/_created/"
             target="_blank"
             rel="noopener noreferrer"
-            className="sm:text-lg text-md bg-white/50 p-2 rounded-full flex items-center text-zinc-700"
+            className="sm:text-lg xs:text-md text-sm bg-white/50 p-2 rounded-full flex items-center text-zinc-700"
           >
             <FaPinterest className="inline-block" />
           </a>
           <a
             href="mailto:simplytogetherevents@gmail.com"
-            className="sm:text-lg text-md bg-white/50 p-2 rounded-full flex items-center text-zinc-700"
+            className="sm:text-lg xs:text-md text-sm bg-white/50 p-2 rounded-full flex items-center text-zinc-700"
           >
             <FaEnvelope className="inline-block" />
           </a>
@@ -32,7 +32,7 @@ const Footer = () => {
 
         <span className="border-b-[1px] border-white/50 w-20 my-4"></span>
 
-        <p className="sm:mb-8 mx-4 mb-6 text-white/50 text-xs sm:text-sm text-center">
+        <p className="sm:mb-8 mx-4 xs:mb-6 mb-4 text-white/50 text-[10px] xs:text-xs sm:text-sm text-center">
           Copyright &copy; {new Date().getFullYear()} Simply Togehter Events LLC
           - All Rights Reserved.
         </p>
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,35 +11,35 @@ import photo5 from "./photo5.jpg";
 const Homepage = () => {
   return (
     <div>
-      <div className="relative overflow-hidden h-[65vh] mt-12">
-        <div className="relative flex items-center max-w-full h-4/6">
+      <div className="relative overflow-hidden md:h-[65vh] mt-12">
+        <div className="relative flex items-center max-w-full md:h-4/6">
           <img
-            className="animate-slideup  relative top-14 left-0 w-80 object-cover"
+            className="animate-slideup relative md:top-14 xs:bottom-3 left-0 md:w-80 xs:w-48 w-28 object-cover"
             src={photo0}
             alt="ste1"
           />
           <img
-            className="animate-slidedown  relative w-60 top-8 right-10 z-20 object-cover"
+            className="animate-slidedown relative md:w-60 xs:w-28 w-16 md:top-8 xs:bottom-8 bottom-4 xs:right-10 right-4 z-20 object-cover"
             src={photo1}
             alt="ste2"
           />
           <img
-            className="animate-slideup  relative w-56 top-32 right-16 z-10 object-cover"
+            className="animate-slideup relative md:w-56 xs:w-24 w-14 md:top-32 xs:top-12 top-6 xs:right-16 right-6 z-10 object-cover"
             src={photo2}
             alt="ste3"
           />
           <img
-            className="animate-slidedown  relative w-96 object-cover top-16 right-32"
+            className="animate-slidedown relative md:w-96 xs:w-56 w-32 object-cover md:top-16 md:right-32 right-10 xs:right-24"
             src={photo3}
             alt="ste4"
           />
           <img
-            className="w-64 animate-slideup  object-cover relative z-10 top-20 right-40"
+            className="md:w-64 xs:w-36 w-20 animate-slideup object-cover relative z-10 md:top-20 xs:top-2 md:right-40 right-14 xs:right-32"
             src={photo4}
             alt="ste5"
           />
           <img
-            className="w-64 animate-slidedown  object-cover relative top-4 right-56"
+            className="md:w-64 xs:w-36 w-16 animate-slidedown object-cover relative md:top-4 xs:bottom-10 bottom-8 md:right-56 xs:right-36 right-20"
             src={photo5}
             alt="ste6"
           />
@@ -48,17 +48,17 @@ const Homepage = () => {
         <img
           src={logo}
           alt="simplytogetherevents"
-          className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-10/12 z-40"
+          className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-screen sm:w-10/12 z-40"
         />
       </div>
 
-      <div className="text-center mt-4 animate-fadein">
-        <p className="font-normal text-2xl">
+      <div className="text-center mt-4 mx-6 animate-fadein">
+        <p className="tracking-wide text-md font-medium sm:text-lg md:font-normal md:text-xl lg:font-normal lg:text-2xl">
           simply bringing everyone together the most beautiful way we know how.
         </p>
         <Link
           to="/services"
-          className="font-medium mt-4 static p-3 bg-[#A4133C] hover:bg-[#800F2F] active:scale-90 inline-block text-white rounded-md"
+          className="font-medium lg:text-base sm:text-sm text-xs mt-4 p-2 sm:p-3 bg-[#A4133C] hover:bg-[#800F2F] active:scale-90 inline-block text-white rounded-md"
         >
           learn more
         </Link>
