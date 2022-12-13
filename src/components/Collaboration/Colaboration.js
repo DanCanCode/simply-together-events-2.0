63c28758ae176a0df1138f626978e34120cdd19d
Daniel Espinal <dancancode@protonmail.com>
2022-12-13 15:42:12 -0500

    collaboration page done

--- src/components/Collaboration/Colaboration.js
+++ src/components/Collaboration/Colaboration.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import backgroundVideo from "./background.mp4";
 
 const Collaboration = () => {
   useEffect(() => {
@@ -49,141 +50,212 @@ const Collaboration = () => {
   }, []);
 
   return (
-    <main className="container mx-auto animate-[fadein_2s_ease_1]">
-      <div className="max-w-lg mx-auto my-10 bg-white p-5 rounded-lg shadow-lg">
-        <div className="text-center">
-          <h1 className="my-3 text-3xl font-semibold text-black">
-            Collaboration
-          </h1>
-          <p className="text-black/50">
-            Please fill out the form below and we will get back to you!
-          </p>
+    <>
+      {/* <div className="h-screen w-full overflow-hidden">
+        <video
+          autoPlay
+          muted
+          loop
+          className="object-cover object-center w-full h-full"
+        >
+          <source src={backgroundVideo} type="video/mp4" />
+        </video>
+      </div> */}
+
+      <main className="container mx-auto animate-[fadein_2s_ease_1]">
+        <div className="max-w-xl mx-auto my-10 bg-white p-5 rounded-lg shadow-lg">
+          <div className="text-center">
+            <h1 className="my-3 text-3xl font-semibold text-black">
+              Collaboration
+            </h1>
+            <p className="text-black/50 sm:mx-20 xs:mx-10 text-center">
+              Looking to collaborte with us? Fill out the form and we will be in
+              touch!
+            </p>
+          </div>
+
+          <section className="m-7">
+            <form
+              action="https://api.web3forms.com/submit"
+              method="POST"
+              id="form"
+            >
+              <input
+                type="hidden"
+                name="access_key"
+                value="YOUR_ACCESS_KEY_HERE"
+              />
+              <input
+                type="hidden"
+                name="subject"
+                value="New Submission from your Website"
+              />
+              <input
+                type="checkbox"
+                name="botcheck"
+                id=""
+                style={{ display: "none" }}
+              />
+
+              <div className="mb-6 grid grid-cols-2 gap-2 content-center">
+                <div>
+                  <label
+                    htmlFor="firstName"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                    placeholder="John"
+                    required
+                    className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Doe"
+                    required
+                    className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+              </div>
+
+              <div className="mb-6 grid grid-cols-2 gap-2 content-center">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="you@company.com"
+                    required
+                    className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="phone"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="+1 (555) 1234-567"
+                    required
+                    className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+              </div>
+
+              <div className="mb-6 grid grid-cols-2 gap-2 content-center">
+                <div>
+                  <label
+                    htmlFor="instagram"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    Your Instagram
+                  </label>
+                  <input
+                    type="text"
+                    name="instagram"
+                    id="instagram"
+                    placeholder="https://www.instagram.com/yourpagename/"
+                    required
+                    className="placeholder:truncate w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="tiktok"
+                    className="block mb-2 text-sm text-black/40"
+                  >
+                    Your TikTok
+                  </label>
+                  <input
+                    type="text"
+                    name="tiktok"
+                    id="tiktok"
+                    placeholder="https://www.tiktok.com/@yourpagename"
+                    required
+                    className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  />
+                </div>
+              </div>
+
+              <div className="mb-6">
+                <label
+                  htmlFor="location"
+                  className="block mb-2 text-sm text-black/40"
+                >
+                  Your Location
+                </label>
+                <input
+                  type="text"
+                  name="location"
+                  id="location"
+                  placeholder="New York"
+                  required
+                  className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                />
+              </div>
+
+              <div className="mb-6">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm text-black/40"
+                >
+                  Your Message
+                </label>
+
+                <textarea
+                  rows="5"
+                  name="message"
+                  id="message"
+                  placeholder="Your Message"
+                  className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
+                  required
+                ></textarea>
+              </div>
+              <div className="mb-6">
+                <button
+                  type="submit"
+                  className="w-full px-3 py-4 text-white tracking-wide font-medium rounded-md bg-[#A4133C] hover:bg-[#800F2F] active:scale-90 focus:outline-none"
+                >
+                  Send Message
+                </button>
+              </div>
+              <p
+                className="text-base text-center text-gray-500"
+                id="result"
+              ></p>
+            </form>
+          </section>
         </div>
-        <section className="m-7">
-          <form
-            action="https://api.web3forms.com/submit"
-            method="POST"
-            id="form"
-          >
-            <input
-              type="hidden"
-              name="access_key"
-              value="YOUR_ACCESS_KEY_HERE"
-            />
-            <input
-              type="hidden"
-              name="subject"
-              value="New Submission from your Website"
-            />
-            <input
-              type="checkbox"
-              name="botcheck"
-              id=""
-              style={{ display: "none" }}
-            />
-
-            <div className="mb-6">
-              <label
-                htmlFor="name"
-                className="block mb-2 text-sm text-black/40"
-              >
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="John Doe"
-                required
-                className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm text-black/40"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="you@company.com"
-                required
-                className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="phone"
-                className="block mb-2 text-sm text-black/40"
-              >
-                Phone Number
-              </label>
-              <input
-                type="text"
-                name="phone"
-                id="phone"
-                placeholder="+1 (555) 1234-567"
-                required
-                className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
-              />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="event" className="block mb-2 text-black/40">
-                Event Type
-              </label>
-
-              <select
-                id="event"
-                name="event"
-                required
-                defaultValue={"default"}
-                className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
-              >
-                <option value="default" disabled>
-                  Choose event type
-                </option>
-                <option value="Event Planning">Event Planning</option>
-                <option value="Event Design">Event Design</option>
-                <option value="Luxury Picnic">Luxury Picnic</option>
-                <option value="Rental">Rental</option>
-                <option value="Event Content Creation">
-                  Event Content Creation
-                </option>
-              </select>
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="message"
-                className="block mb-2 text-sm text-black/40"
-              >
-                Your Message
-              </label>
-
-              <textarea
-                rows="5"
-                name="message"
-                id="message"
-                placeholder="Your Message"
-                className="w-full px-3 py-2 placeholder-black/30 border border-black/20 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-300"
-                required
-              ></textarea>
-            </div>
-            <div className="mb-6">
-              <button
-                type="submit"
-                className="w-full px-3 py-4 text-white tracking-wide font-medium rounded-md bg-[#A4133C] hover:bg-[#800F2F] active:scale-90 focus:outline-none"
-              >
-                Send Message
-              </button>
-            </div>
-            <p className="text-base text-center text-gray-500" id="result"></p>
-          </form>
-        </section>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
